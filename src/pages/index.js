8bb54624570601937f694863911407610b17be96
Arushi Aggarwal <aggarwalarushi5@gmail.com>
2021-07-16 16:58:02 +0530

    error  fix #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Layout from '../components/Layout'
-import home from '../styles/home.module.css'
+import Layout from '../components/Layout';
+import styles from '../styles/home.module.css';
 
 export default function Home() {
   return (
     <Layout>
-    <section className={home.header}>
+    <section className={styles.header}>
         <div>
           <h4>Hi, my name is</h4>
           <h2>Arushi</h2>
