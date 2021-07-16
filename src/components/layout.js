a026bb13a137b2210a714701fb234916b4bad971
Arushi Aggarwal <aggarwalarushi5@gmail.com>
2021-07-16 13:56:00 +0530

    barebones layout

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Layout() {
+    retrurn (
+        <div className= "layout">
+        <div className = "content">
+            {/* content for each component will be seperated*/ }
+        </div>
+        <footer>
+            <p>Styled and Programmed by Arushi Aggarwal</p>
+            <p>Copyright 2021 Arushi Aggarwal</p>
+        </footer>
+
+        </div>
+    )
+}
