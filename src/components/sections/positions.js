95d427baaa699c8e629a703a9f40a7c48443d1a1
Arushi Aggarwal <aggarwalarushi5@gmail.com>
2021-07-31 16:30:48 +0530

    fix bug

--- src/components/sections/positions.js
+++ src/components/sections/positions.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState } from "react";
 import { graphql, useStaticQuery } from 'gatsby';
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 
 export default function Positions() {
-    const data = useStaticQuery(graphql`
+    const pdata = useStaticQuery(graphql`
     query{
         positions: allMarkdownRemark(
             filter: {fileAbsolutePath: {regex: "/positions"}}
@@ -19,10 +21,42 @@ export default function Positions() {
         }
     }
     `)
+    const posData = pdata.positions.edges;
     return(
         <section>
         <h2 className = "heading">Achievements & Positions of Responsibility</h2>
-
+        <AnimateSharedLayout>
+            <motion.ul layout>
+            {posData && posData.map(({ node }, i ) => (
+            <Item key={i} />
+            ))}
+            </motion.ul>
+        </AnimateSharedLayout>
         </section>
+    )
+}
+
+function Item() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = () => setIsOpen(!isOpen);
+    return (
+        <motion.li layout onClick= {toggleOpen} inital = {{ borderRadius: 5 }}>
+            <motion.div  layout/>
+            <AnimatePresence> {isOpen && <Content />}</AnimatePresence>
+        </motion.li>
+    );
+}
+
+function Content() {
+    return (
+        <motion.div 
+        layout
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}  
+        >
+            <div />
+        </motion.div>
     )
 }
