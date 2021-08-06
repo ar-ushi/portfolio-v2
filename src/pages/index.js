413c87db84900a0c16d2d7da307a038c3ff39bf9
Arushi Aggarwal <aggarwalarushi5@gmail.com>
2021-08-06 14:37:15 +0530

    added section for projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import React from "react"
 import Layout from '../components/Layout';
-import * as style from '../styles/home.module.css';
-import {email} from '../config';
-import { Icon } from "../components/ui-components/icons";
 import AboutMe from "../components/sections/about";
 import Experience from "../components/sections/experience";
 import Positions from "../components/sections/positions";
