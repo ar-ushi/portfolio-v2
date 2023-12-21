89fb31ebbed081e2fb7977077a1a5c3d9c479526
Arushi <aggarwalarushi5@gmail.com>
2023-12-21 21:10:29 +0530

    minor content change

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
             <h4 className= {style.name}>Hey! I'm</h4>
             <h2 className = {style.bigH}>Arushi Aggarwal.</h2>
             <div>
-                <p className={style.headingDescription}>I'm currently working at American Express as a Digital Product Management - Associate and I am looking to switch into a Software Engineer Role.</p>
+                <p className={style.headingDescription}>I'm currently working at American Express as a Digital Product Management Associate and I am looking to switch into a Software Engineer Role.</p>
             </div>
             <div className= {style.btnwrapper}>
                 <a href={`mailto:${email}`} className={style.btn}>
