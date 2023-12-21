28d1d907c6dbe8f6245a8245050bf22a5f2c5207
Arushi <aggarwalarushi5@gmail.com>
2023-12-22 00:17:43 +0530

    refactor links and website code - make css same

--- src/components/ui-components/links.jsx
+++ src/components/ui-components/links.jsx
@@ -1,62 +1,14 @@
 import React from 'react';
 import { socialMediaLinks } from '../../config';
+import * as style from '../../styles/home.module.css'
 import { Icon } from './icons';
 import Side from './side';
-import styled from 'styled-components';
 
-const StyledLinkList = styled.ul`
-display:flex;
-flex-direction: column;
-margin: 0;
-padding: 0;
-list-style: none;  
-&:after  {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 100px;
-    margin: 0 auto;
-    background-color: var(--primary);
-  }
-  &:before  {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 90vh;
-    margin: 0 auto;
-    background-color: var(--primary);
-  }
 
-  li {
-    margin-bottom: 24px;
-    &:last-of-type {
-      margin-bottom: 20px;
-    }
-
-    a {
-      padding: 10px;
-      color : var(--white);
-
-      &:hover,
-      &:focus {
-        transition: var(--transition-link);
-        transform: translateY(-3px);
-        svg{
-          stroke: var(--primary);
-        }
-      }
-
-      svg {
-        width: 20px;
-        height: 20px;
-      }
-    }
-  }
-`;
 export default function Links() {
     return (
         <Side orientation = "left">
-        <StyledLinkList>
+        <ul className={style.listlinks}>
         {socialMediaLinks && socialMediaLinks.map(( {url, name}, i) => (
             <li key={i}>
                 <a href={url}  aria-label={name} target="_blank" rel="noreferrer">
@@ -64,9 +16,7 @@ export default function Links() {
                 </a>
             </li>
         ))}
-        </StyledLinkList>
+        </ul>
     </Side>
-
-
     )
 }
--- src/components/ui-components/website.jsx
+++ src/components/ui-components/website.jsx
@@ -1,57 +1,17 @@
 import React from 'react';
 import { portfolio } from '../../config';
 import Side from './side';
-import styled from 'styled-components';
-
-const StyledLinkList = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-position: relative;
-
-&:after {
-  content: '';
-  display: block;
-  width: 1px;
-  height: 90px;
-  margin: 0 auto;
-  background-color: var(--primary);
-}
-
-&:before  {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 90vh;
-    margin: 0 auto;
-    background-color: var(--primary);
-    }
+import * as style from '../../styles/home.module.css'
 
 
-a {
-  margin: 20px auto;
-  padding: 10px;
-  font-size: 22px;
-  color: var(--light-gray);
-  letter-spacing: 0.1em;
-  writing-mode: vertical-rl;
-
-  &:hover{
-    color: var(--primary);
-    transition: all 200ms ease-in;
-    transform: translateY(-3px);
-  }
-  
-}
-`;
 export default function Website() {
     return (
         <Side orientation = "right">
-        <StyledLinkList>
+        <ul className={style.listlinks}>
                 <a href={`${portfolio}`} target="_blank" rel="noreferrer">
                     check out v1 of my portfolio!
                 </a>
-        </StyledLinkList>
+        </ul>
     </Side>
     )
 }
